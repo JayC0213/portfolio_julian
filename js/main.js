38d884ecce0d0d577c0a80eabc791b3cacf379b2
JayC0213 <juliancabadas@yahoo.com>
2025-04-13 22:35:51 -0700

    updated

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let future = document.getElementById('futureSound');
+let home = document.getElementById('home-page');
+let resume = document.getElementById('resume-page');
+let linkedin = document.getElementById('linkedin-profile');
+let picture1 = document.getElementById('hoveringImage1');
+let picture2 = document.getElementById('hoveringImage2');
+let picture3 = document.getElementById('hoveringImage3');
+let picture4 = document.getElementById('hoveringImage4');
+let picture5 = document.getElementById('hoveringImage5');
+
+picture1.addEventListener("mouseover", () => {
+    future.play();
+});
+
+picture2.addEventListener("mouseover", () => {
+    future.play();
+});
+
+picture3.addEventListener("mouseover", () => {
+    future.play();
+});
+
+picture4.addEventListener("mouseover", () => {
+    future.play();
+});
+
+picture5.addEventListener("mouseover", () => {
+    future.play();
+});
+
+home.addEventListener("mouseover", () => {
+    future.play();
+});
+
+resume.addEventListener("mouseover", () => {
+    future.play();
+});
+
+linkedin.addEventListener("mouseover", () => {
+    future.play();
+});
+
